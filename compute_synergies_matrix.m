25c13b53897377197f679431a1fc20b802fbd28f
penguinscontrol <penguinscontrol@gmail.com>
2015-01-21 19:38:17 -0500

    bugfix

--- compute_synergies_matrix.m
+++ compute_synergies_matrix.m
@@ -1,4 +1,4 @@
-function [out, comp_c_sca, comp_t_del, save_new_err] = compute_synergies_matrix( varargin )
+function [out, comp_c_sca, comp_t_del, save_err] = compute_synergies_matrix( varargin )
 %compute_synergies.m given a set of observed episodes eps = varargin{1},
 %computes the N = varargin{2} time varying synergies of length
 %T = varargin{3} that best explain the observations based on an
@@ -58,10 +58,19 @@ derr_tol = 1e-4;
 % if error not changing by more than derr_tol, stop.
 % maximum number of allowed iterations
 maxiterations = 500;
-save_new_err = zeros(1, maxiterations);
+% Get first error estimate
+    curr_rec_err = 0;
+    for s = 1:N_eps
+    [this_rec_err, ~] = ...
+        reconstruction_error(eps{s}, out, c_sca(s,:), t_del(s,:));
+    curr_rec_err = curr_rec_err+this_rec_err;
+    end
+    save_err = sqrt(curr_rec_err); % the total reconstruction error at the current point
+
 
 % initialize gradient norm, optimization vector, iteration counter, perturbation
 niter = 1; new_err = inf; derr = inf;
+
 while (new_err>=err_tol && niter <= maxiterations && abs(derr) > derr_tol) 
     % continue iterating until done
     tic
@@ -88,21 +97,28 @@ while (new_err>=err_tol && niter <= maxiterations && abs(derr) > derr_tol)
     end
     fprintf('Updated delays for iteration %d\n', niter);
     
-    H_all = [];
+    H_all = []; 
     for s = 1:N_eps
+        
+        this_h = h_mat(c_sca(s,:), t_del(s,:), N, T, ep_lengths(s));
+     
         for ii = 1:N
             this_theta = theta_mat(ii, t_del(s,ii), N, T, ep_lengths(s));
-            this_h = h_mat(c_sca(s,:), t_del(s,:), N, T, ep_lengths(s));
             c_sca(s,ii) = c_sca(s,ii)...
-                *trace(M_s{s}'*W_mat*this_theta)...
-                /trace(this_h'*(W_mat')*W_mat*this_theta);
+                *trace( (M_s{s}') * W_mat * this_theta)...
+                /trace( (this_h') *(W_mat') * W_mat * this_theta);
         end
+        
+        % Change H_s to reflect new scaling coeffs
+        this_h = h_mat(c_sca(s,:), t_del(s,:), N, T, ep_lengths(s));
+
         H_all = [H_all this_h];
     end % end cycle episodes
     
     fprintf('Updated scales for iteration %d\n', niter);
+    
     %% update the synergy elements by gradient descent, one value at a time
-    new_W_mat = W_mat * ( (M_all * (H_all') ) \ (W_mat * H_all * (H_all') ));
+    new_W_mat = W_mat .* ( (M_all * (H_all') ) ./ (W_mat * H_all * (H_all') ));
     
     new_out = cell(N,1);
     for ii = 1:N
@@ -120,15 +136,20 @@ while (new_err>=err_tol && niter <= maxiterations && abs(derr) > derr_tol)
     end
     new_err = sqrt(curr_rec_err); % the total reconstruction error at the current point
 
-    % update termination metrics
-    save_new_err(niter) = new_err;
+    
     if niter > 1
-        if new_err < save_new_err(niter - 1) % Update if new synergies are better
+        if new_err < save_err(end) % Update if new synergies are better
             out = new_out;
+            W_mat = new_W_mat;
+        % update termination metrics
         else
-            fprintf('WARNING: new error more than old. Skipping assignment.\n')
+            fprintf('              WARNING:       new error more than old.\n');
+            out = new_out;
+            W_mat = new_W_mat;
         end
-        derr = save_new_err(niter) - save_new_err(niter-1);
+        
+            derr = save_err(end) - save_err(end-1);
+            
         fprintf('Error is: %4.4f\n',new_err);
         fprintf('delta error is: %4.4f\n',derr);
     else
@@ -136,13 +157,19 @@ while (new_err>=err_tol && niter <= maxiterations && abs(derr) > derr_tol)
         % do you know what error tolerance to expect. This runs only once,
         % during the first iteration.
         out = new_out;
+        W_mat = new_W_mat;
+        
         err_tol = new_err;
         fprintf('Initial error is: %4.4f\n',err_tol);
-        err_tol = 0.005.*err_tol;
-        derr_tol = 0.0001.*err_tol;
+        err_tol = 0.05.*err_tol;
+        derr_tol = 0.001.*err_tol;
     end
     
     fprintf('Finished iteration %d\n', niter);
+    
+        % update error log
+        save_err = [save_err new_err];
+        
     niter = niter + 1;
     toc
 end
--- theta_mat.m
+++ theta_mat.m
@@ -2,7 +2,9 @@ function theta = theta_mat(ii, tau, N, T, T_ep)
 theta = zeros(N*T,T_ep);
 for p = 1:N*T
     for q = 1:T_ep
-       theta(p,q) = (p-(ii-1)*T) == (q-tau);
-    end 
+        if p > (ii-1)*T && p < ii*T+1
+            theta(p,q) = (p-(ii-1)*T) == (q-tau);
+        end
+    end
 end
 end
