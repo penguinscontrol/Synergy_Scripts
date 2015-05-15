339df69275b6895fee0e7cbda0ca90c0835acb4d
penguinscontrol <penguinscontrol@gmail.com>
2015-05-15 10:11:05 -0400

    minor

--- rfig.m
+++ rfig.m
@@ -3,7 +3,7 @@ function [ out] = rfig
 
 out = figure;
 screenSize = get(0, 'ScreenSize');
-set(gcf, 'position', [screenSize(3:4).*[1.7, 1]*0.1 screenSize(3:4).*[1, 1.3]*0.6]);
-
+set(out, 'position', [screenSize(3:4).*[1.7, 1]*0.1 screenSize(3:4).*[1, 1.3]*0.6]);
+set(out, 'PaperPositionMode', 'auto')
 end
 
